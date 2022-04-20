c2651d798cc91b536b536d954392c04801d7c5b3
Alejandracharris <98917701+Alejandracharris@users.noreply.github.com>
2022-04-20 15:46:38 -0500

    Update meeting.py

--- meeting.py
+++ meeting.py
@@ -1,6 +1,6 @@
 from item import Item
 class Meeting(Item):
-    def__init__(self,link: str, meeting_date: date) -> None:
+    def__init__(self,link: str, meeting_date: date, description: str, creation_date: date, relevance_level: int) -> None:
         super.__init__(description: str, creation_date: date, relevence_level: int)
         
         self.link = link
