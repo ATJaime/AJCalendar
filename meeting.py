a9e705bdfd84b48e0490b112b01e66b8d9075e9b
ATJaime <84157609+ATJaime@users.noreply.github.com>
2022-04-20 16:14:05 -0500

    Update meeting.py

--- meeting.py
+++ meeting.py
@@ -1,10 +1,19 @@
 from item import Item
+from datetime import date
 class Meeting(Item):
-    def__init__(self,link: str, meeting_date: date, description: str, creation_date: date, relevance_level: int) -> None:
-        super.__init__(description: str, creation_date: date, relevence_level: int)
-        
+    def __init__(self, 
+        link: str, 
+        meeting_date: date, 
+        description: str, 
+        creation_date: date, 
+        relevance_level: int
+    ) -> None:
+        super.__init__(description=description,
+            creation_date=creation_date,
+            relevance_level=relevance_level
+        )
         self.link = link
         self.meeting_date = meeting_date
     
-    def cancel_meeting(self) -> None:
+    def cancel_meeting(self) -> None
         pass
