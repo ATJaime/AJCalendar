b4271f8339f0d1029be610a234c3bacf6be61b35
Alejandracharris <98917701+Alejandracharris@users.noreply.github.com>
2022-04-20 15:33:55 -0500

    Update note.py

--- note.py
+++ note.py
@@ -1,3 +1,21 @@
 from item import Item
 class Note(Item):
-    pass
+    def__init__(self,item: Item, font: str, font_size: float, font_color:  color, background_color: color) -> None
+        self.item = item
+        self.font = font
+        self.font_size = font_size
+        self.font_color = font_size
+        self.background_color = background
+       
+    def set_font(self) ->None
+        pass
+    
+    def set_font_size(self) -> None
+        pass
+    
+    def set_font_color(self) -> None
+        pass
+    
+    def set_background_color(self) -> None
+        pass
+    
