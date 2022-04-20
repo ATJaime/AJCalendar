7b3e32cb9866707eba7b5eeb403d37b5d6d81d8a
ATJaime <84157609+ATJaime@users.noreply.github.com>
2022-04-20 16:13:05 -0500

    Update container.py

--- container.py
+++ container.py
@@ -1,2 +1,9 @@
-class Container:
-    pass
+from modify import Modify
+from task import Task
+from note import note
+from meeting import Meeting
+class Container(Modify):
+    def __init__(self):
+        self.notes = []
+        self.tasks = []
+        self.meetings = []
