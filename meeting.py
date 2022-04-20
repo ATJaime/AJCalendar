c0584b65498366a6e11f865fe0a503ca65a7c012
Alejandracharris <98917701+Alejandracharris@users.noreply.github.com>
2022-04-20 15:23:25 -0500

    Update meeting.py

--- meeting.py
+++ meeting.py
@@ -1,3 +1,9 @@
 from item import Item
 class Meeting(Item):
-    pass
+    def__init__(self,Item, link: str, meeting_date: date) -> None
+        self.Item = item
+        self.link = link
+        self.meeting_date = meeting_date
+    
+    def cancel_meeting(self) -> None
+        pass
