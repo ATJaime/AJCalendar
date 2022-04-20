52ff8ea9f46a51af13cee83acb83457da5f0ae6e
Alejandracharris <98917701+Alejandracharris@users.noreply.github.com>
2022-04-20 15:17:04 -0500

    Update item.py

--- item.py
+++ item.py
@@ -2,4 +2,14 @@ from datetime import date
 
 class Item:
     def __init__(self, description: str, creation_date: date, relevance_level: int) -> None:
+       
         ...
+    def Update(self) -> None:
+        pass
+
+    def Delate(self) -> None:
+        pass
+
+    def Random_show(self) -> None:
+        pass
+    
