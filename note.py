03117dcd53129003984658e1629f07c63a1ea2bd
ATJaime <84157609+ATJaime@users.noreply.github.com>
2022-04-20 16:15:57 -0500

    Update note.py

--- note.py
+++ note.py
@@ -1,8 +1,18 @@
 from item import Item
 class Note(Item):
-    def__init__(self,font: str, font_size: float, font_color:  color, background_color: color) -> None:
-        super.__init__(description =  str, creation_date =  date, relevance_level =  int)
-        
+    def __init__(self,
+        font: str, 
+        font_size: float, 
+        font_color:  color, 
+        background_color: color,
+        description: str, 
+        creation_date: date, 
+        relevance_level: int
+    ) -> None:
+        super.__init__(description=description, 
+            creation_date=creation_date, 
+            relevance_level=relevance_level
+        )
         self.font = font
         self.font_size = font_size
         self.font_color = font_size
@@ -19,4 +29,3 @@ class Note(Item):
     
     def set_background_color(self) -> None:
         pass
-    
