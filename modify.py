7b2373170bb92195cb7af91d39eca3ed9b7e98a7
Alejandracharris <98917701+Alejandracharris@users.noreply.github.com>
2022-04-20 15:53:21 -0500

    Update modify.py

--- modify.py
+++ modify.py
@@ -1,2 +1,31 @@
 class Modify:
-    pass
+    
+    def__init__(self) -> None:
+        pass
+    
+    def create_task(self):
+        pass
+    
+    def create_meeting(self):
+        pass
+    
+    def create_note(self):
+        pass
+    
+    def block_distractions(self):
+        pass
+    
+    def remove_task(self):
+        pass
+    
+    def remove_meeting(self):
+        pass
+    
+    def remove_note(self):
+        pass
+    
+    def unblock_distractions(self):
+        pass
+    
+    def update_preferences(self):
+        pass
